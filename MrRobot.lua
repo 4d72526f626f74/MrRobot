effccaf6c5eadde15ed1c382b69838e354536d93
4d72526f626f74 <92627754+4d72526f626f74@users.noreply.github.com>
2023-07-29 15:58:28 +0100

    Add files via upload

--- MrRobot.lua
+++ MrRobot.lua
@@ -403,7 +403,7 @@ pluto_class MrRobot
     end
 end
 
-local Script = pluto_new MrRobot('1.4.1', '1.67')
+local Script = pluto_new MrRobot('1.4.2', '1.67')
 Script:CheckGameVersion()
 Script:SetupPackagePath()
 Script:FixMissingDirs()
